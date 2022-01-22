b0facdb2fd89a3e91afd942a332c74fd0e678807
robconceicao <robtconceicao@gmail.com>
2022-01-22 15:35:30 -0300

    c04 - criando funções

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -41,67 +41,17 @@ for(var i = 0; i < pacientes.length; i++) {
   }
 
   if(alturaEhValida && pesoEhValido) {
-
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc.toFixed(2);
+    var imc = calculaImc(peso,altura);
+    tdImc.textContent = imc;
 
   }
 
 }
 
-<<<<<<< HEAD:js/calcula-imc.js
+function calculaImc(peso,altura) {
+  var imc = 0;
 
-=======
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
+  imc = peso / (altura * altura);
+  return imc.toFixed(2);
 
-    var form = document.querySelector("#form-adiciona");
-
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-
-});
->>>>>>> 0f44be51525ae6ffc412ebc3fb0381af9612a54c:js/principal.js
-
-/* titulo.addEventListener("click", function() {
-
-  console.log("fui clicado");
-
-});*/
-
-//podemos usar funções nomeadas ou funções anônimas
-//função anônima
-/*titulo.addEventListener("click", function(){
-
-});*/
-
-//função nomeada
-/*
-function mostraMensagem(){
-  console.log("Olá eu fui clicado!");
-}*/
+}
