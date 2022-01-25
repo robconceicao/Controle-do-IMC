ce2a4fe1f1a769a8bcb231476d1d8561ddcf7770
robconceicao <robtconceicao@gmail.com>
2022-01-25 11:51:33 -0300

    c05 - criando funções

--- js/form.js
+++ js/form.js
@@ -3,12 +3,13 @@ botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
-
+    //extraindo informações do paciente do form
     var nome = form.nome.value;
     var peso = form.peso.value;
     var altura = form.altura.value;
     var gordura = form.gordura.value;
 
+    //cria a tr e a td do paciente
     var pacienteTr = document.createElement("tr");
 
     var nomeTd = document.createElement("td");
@@ -29,6 +30,7 @@ botaoAdicionar.addEventListener("click", function(event) {
     pacienteTr.appendChild(gorduraTd);
     pacienteTr.appendChild(imcTd);
 
+    //adicionando o paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
